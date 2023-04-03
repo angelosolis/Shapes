eaff92fd486e3517bf23e23c96f69737fcd25887
QQ139 <QQ139@CSLAB216>
2023-04-03 09:03:35 +0800

    Add project files.

---
+++ Shapes/Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace Shapes
+{
+    partial class frmShape
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rdbRectangle = new System.Windows.Forms.RadioButton();
+            this.rdbSquare = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.txtSquareSide = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtSides = new System.Windows.Forms.TextBox();
+            this.btnDisplay = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtLength = new System.Windows.Forms.TextBox();
+            this.txtWidth = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rdbSquare);
+            this.groupBox1.Controls.Add(this.rdbRectangle);
+            this.groupBox1.Location = new System.Drawing.Point(58, 36);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Shape";
+            // 
+            // rdbRectangle
+            // 
+            this.rdbRectangle.AutoSize = true;
+            this.rdbRectangle.Location = new System.Drawing.Point(22, 28);
+            this.rdbRectangle.Name = "rdbRectangle";
+            this.rdbRectangle.Size = new System.Drawing.Size(74, 17);
+            this.rdbRectangle.TabIndex = 1;
+            this.rdbRectangle.TabStop = true;
+            this.rdbRectangle.Text = "Rectangle";
+            this.rdbRectangle.UseVisualStyleBackColor = true;
+            this.rdbRectangle.CheckedChanged += new System.EventHandler(this.rdbRectangle_CheckedChanged);
+            // 
+            // rdbSquare
+            // 
+            this.rdbSquare.AutoSize = true;
+            this.rdbSquare.Location = new System.Drawing.Point(22, 51);
+            this.rdbSquare.Name = "rdbSquare";
+            this.rdbSquare.Size = new System.Drawing.Size(59, 17);
+            this.rdbSquare.TabIndex = 1;
+            this.rdbSquare.TabStop = true;
+            this.rdbSquare.Text = "Square";
+            this.rdbSquare.UseVisualStyleBackColor = true;
+            this.rdbSquare.CheckedChanged += new System.EventHandler(this.rdbSquare_CheckedChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtSides);
+            this.groupBox2.Controls.Add(this.txtName);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Location = new System.Drawing.Point(292, 36);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 100);
+            this.groupBox2.TabIndex = 0;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Shape";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.txtSquareSide);
+            this.groupBox3.Controls.Add(this.label1);
+            this.groupBox3.Location = new System.Drawing.Point(292, 192);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(200, 100);
+            this.groupBox3.TabIndex = 1;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Square";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Length";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.txtWidth);
+            this.groupBox4.Controls.Add(this.txtLength);
+            this.groupBox4.Controls.Add(this.label5);
+            this.groupBox4.Controls.Add(this.label6);
+            this.groupBox4.Location = new System.Drawing.Point(58, 192);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(200, 100);
+            this.groupBox4.TabIndex = 1;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Rectangle";
+            // 
+            // txtSquareSide
+            // 
+            this.txtSquareSide.Location = new System.Drawing.Point(64, 30);
+            this.txtSquareSide.Name = "txtSquareSide";
+            this.txtSquareSide.Size = new System.Drawing.Size(100, 20);
+            this.txtSquareSide.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 60);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Sides";
+            this.label3.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(23, 30);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Name";
+            this.label4.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(64, 28);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 2;
+            // 
+            // txtSides
+            // 
+            this.txtSides.Location = new System.Drawing.Point(64, 57);
+            this.txtSides.Name = "txtSides";
+            this.txtSides.Size = new System.Drawing.Size(100, 20);
+            this.txtSides.TabIndex = 2;
+            // 
+            // btnDisplay
+            // 
+            this.btnDisplay.Location = new System.Drawing.Point(233, 316);
+            this.btnDisplay.Name = "btnDisplay";
+            this.btnDisplay.Size = new System.Drawing.Size(75, 23);
+            this.btnDisplay.TabIndex = 2;
+            this.btnDisplay.Text = "Display";
+            this.btnDisplay.UseVisualStyleBackColor = true;
+            this.btnDisplay.Click += new System.EventHandler(this.btnDisplay_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(22, 61);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Width";
+            this.label5.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(22, 31);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(40, 13);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "Length";
+            this.label6.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtLength
+            // 
+            this.txtLength.Location = new System.Drawing.Point(63, 29);
+            this.txtLength.Name = "txtLength";
+            this.txtLength.Size = new System.Drawing.Size(100, 20);
+            this.txtLength.TabIndex = 2;
+            // 
+            // txtWidth
+            // 
+            this.txtWidth.Location = new System.Drawing.Point(63, 58);
+            this.txtWidth.Name = "txtWidth";
+            this.txtWidth.Size = new System.Drawing.Size(100, 20);
+            this.txtWidth.TabIndex = 2;
+            // 
+            // frmShape
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(528, 406);
+            this.Controls.Add(this.btnDisplay);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "frmShape";
+            this.Text = "Shapes";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rdbSquare;
+        private System.Windows.Forms.RadioButton rdbRectangle;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.TextBox txtSides;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtSquareSide;
+        private System.Windows.Forms.TextBox txtWidth;
+        private System.Windows.Forms.TextBox txtLength;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnDisplay;
+    }
+}
+
---
+++ Shapes/Form1.cs
@@ -0,0 +1,69 @@
+﻿using clsShape;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Shapes
+{
+    public partial class frmShape : Form
+    {
+        public frmShape()
+        {
+            InitializeComponent();
+            groupBox4.Visible = false;
+            groupBox3.Visible = false;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rdbRectangle_CheckedChanged(object sender, EventArgs e)
+        {
+            if(rdbRectangle.Checked)
+            {
+                groupBox4.Visible = true;
+            }
+            else
+            {
+                groupBox4.Visible = false;
+            }
+        }
+
+        private void rdbSquare_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rdbSquare.Checked)
+            {
+                groupBox3.Visible = true;
+            }
+            else
+            {
+                groupBox3.Visible = false;
+            }
+        }
+
+        private void btnDisplay_Click(object sender, EventArgs e)
+        {
+           
+            
+        
+            if(rdbRectangle.Checked)
+            {
+                clnRectangle rt = new clnRectangle(txtName.Text, int.Parse(txtSides.Text), double.Parse(txtLength.Text), double.Parse(txtWidth.Text));
+                MessageBox.Show(rt.ToString());
+            }
+            else if(rdbSquare.Checked)
+            {
+                clnSquare sq = new clnSquare(txtName.Text, int.Parse(txtSides.Text), double.Parse(txtSquareSide.Text));
+                MessageBox.Show(sq.ToString());
+            }
+        }
+    }
+}
